20c3481ab48d4155c48cd7d4ae495ad39bea348e
José Paiva <83441521+beater27032001@users.noreply.github.com>
2025-06-11 02:08:49 -0300

    (feature): coverage tests

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -45,7 +45,7 @@ describe('Register Service', () => {
       password: '123456',
     })
 
-    expect(() =>
+    await expect(() =>
       registerUseCase.execute({
         name: 'John Doe',
         email,
