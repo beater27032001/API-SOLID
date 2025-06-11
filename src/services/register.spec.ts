2361741ef96061301bcd22ac3d0f3aedd4371703
José Paiva <83441521+beater27032001@users.noreply.github.com>
2025-06-11 01:54:41 -0300

    (feature): first test

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,1 +1,33 @@
-import { test } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+
+describe('Register Service', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'jonh@example.com',
+      password: '123456',
+    })
+
+    const isPasswordHashed = await compare('123456', user.password_hash)
+
+    expect(isPasswordHashed).toBe(true)
+  })
+})
