00456416044f228f442aefabc406fd6ab6a0a7f3
José Paiva <83441521+beater27032001@users.noreply.github.com>
2025-06-11 22:24:19 -0300

    (feature): profile use case

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,19 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma } from 'generated/prisma'
+import { Prisma, User } from 'generated/prisma'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
@@ -10,13 +21,6 @@ export class PrismaUsersRepository implements UsersRepository {
       },
     })
 
-    return user
-  }
-
-  async create(data: Prisma.UserCreateInput) {
-    const user = await prisma.user.create({
-      data,
-    })
     return user
   }
 }
