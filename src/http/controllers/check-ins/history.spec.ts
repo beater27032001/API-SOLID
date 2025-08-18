c7b95b2bb7bad26edeb83d7d903803f1a4360701
José Paiva <83441521+beater27032001@users.noreply.github.com>
2025-08-18 00:48:21 -0300

    (feature): update role test

--- src/http/controllers/check-ins/history.spec.ts
+++ src/http/controllers/check-ins/history.spec.ts
@@ -14,7 +14,7 @@ describe('Check-in History (e2e)', () => {
   })
 
   it('should be able to list the history of check-ins', async () => {
-    const { token } = await createAndAuthenticateUser()
+    const { token } = await createAndAuthenticateUser(app, true)
 
     const user = await prisma.user.findFirstOrThrow({
       where: {
--- src/http/controllers/check-ins/metrics.spec.ts
+++ src/http/controllers/check-ins/metrics.spec.ts
@@ -14,7 +14,7 @@ describe('Check-in Metrics (e2e)', () => {
   })
 
   it('should be able to get the count of check-ins', async () => {
-    const { token } = await createAndAuthenticateUser()
+    const { token } = await createAndAuthenticateUser(app, true)
 
     const user = await prisma.user.findFirstOrThrow({
       where: {
